031351af6b5968e3d44d80a99b2c5dc334768033
T8ninho <tonisantos.contato@gmail.com>
2024-06-14 21:45:35 -0300

    master

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,77 @@
-import { collection, getDocs } from "firebase/firestore";
-import { useEffect, useState } from "react"
-import {db} from './firebase/firebase';
+import { collection, deleteDoc, getDocs, doc } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { db, storage } from './firebase/firebase';
 import AudioComp from "./Components/AudioComp";
+import UploadAudio from "./Components/UploadAudio";
+import { deleteObject, ref } from "firebase/storage";
 
-function App() {
+export default function App() {
   const [audios, setAudios] = useState([]);
   const [favoritos, setFavoritos] = useState([]);
   const [play, setPlay] = useState(null);
 
+  const dbName = 'audios';
+
   useEffect(() => {
     const ReceberAudios = async () => {
-      const Resultado = await getDocs(collection(db, 'audios'));
-      const AudioList = Resultado.docs.map(doc => ({id: doc.id, ...doc.data()}));
-      setAudios(AudioList);
-      setFavoritos(AudioList.filter(audio => audio.favorite))
+      const Resultado = await getDocs(collection(db, dbName));
+      const AudioList = Resultado.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+      setAudios(AudioList.filter(audio => !audio.favorite));
+      setFavoritos(AudioList.filter(audio => audio.favorite));
     };
     ReceberAudios();
-  },[])
+  }, []);
+
+  const EnviarAudio = (NovoItem) => {
+    setAudios(Item => [...Item, ...NovoItem]);
+  };
+
+  const Favoritar = async (audio) => {
+    const audioRef = doc(db, dbName, audio.id);
+    const newFavoriteStatus = !audio.favorite;
+    await updateDoc(audioRef, { favorite: newFavoriteStatus });
+    const updatedAudio = { ...audio, favorite: newFavoriteStatus };
+  
+    setAudios(prevAudios =>
+      prevAudios.map(a => a.id === audio.id ? updatedAudio : a)
+    );
+  
+    if (newFavoriteStatus) {
+      setFavoritos(prevFavorites => [...prevFavorites, updatedAudio]);
+    } else {
+      setFavoritos(prevFavorites => prevFavorites.filter(fav => fav.id !== audio.id));
+    }
+  };
+  
+
+  const ExcluirAudio = async (id, fileName) => {
+    // Exclui o documento do Firestore
+    await deleteDoc(doc(db, dbName, id));
+
+    // Exclui o arquivo do Storage
+    const audioRef = ref(storage, `${dbName}/${fileName}`);
+    await deleteObject(audioRef);
+
+    // Atualiza os estados dos áudios
+    setAudios(audios => audios.filter(audio => audio.id !== id));
+    setFavoritos(favoritos => favoritos.filter(audio => audio.id !== id));
+    if (play && play.id === id) {
+      setPlay(null);
+    }
+  };
 
   return (
     <div className="App">
+      <UploadAudio enviando={EnviarAudio} dbName={dbName}/>
       <div>
         <h1>Lista de audios:</h1>
-        <AudioComp audio={audios} onClick={item => setPlay(item)} />
+        <AudioComp audio={audios} onPlay={item => setPlay(item)} onDelete={ExcluirAudio} onFavorite={Favoritar}/>
       </div>
       <div>
         <h1>Favoritos:</h1>
-        <AudioComp audio={favoritos} onClick={item => setPlay(item)} />
+        <AudioComp audio={favoritos} onPlay={item => setPlay(item)} onDelete={ExcluirAudio}  onFavorite={Favoritar}/>
       </div>
       {play && <audio controls src={play.url} autoPlay />}
     </div>
-  )
+  );
 }
-
-export default App
--- src/Components/AudioComp.jsx
+++ src/Components/AudioComp.jsx
@@ -1,14 +1,18 @@
-export default function AudioComp ({audio, onClick}) {
+import { Fragment } from "react";
+
+export default function AudioComp ({audio, onPlay, onDelete, onFavorite}) {
 
 	return(
 		audio.map((item) => (
-			<>
+			<Fragment>
 				<a>{item.name}</a>
 				<div key={item.key}>
-					<button onClick={() => onClick && onClick(item)}>Tocar</button>
+					<button onClick={() => onPlay && onPlay(item)}>Tocar</button>
+					<button onClick={() => onFavorite(item.id)}>Favoritar</button>
+					<button onClick={() => onDelete(item.id, item.name)}>Excluir</button>
 				</div>
 				<hr />
-			</>
+			</Fragment>
 		))
 	);
 }
